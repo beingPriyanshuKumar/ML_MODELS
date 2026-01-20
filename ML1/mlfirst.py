8357f12c605f1221b029c5f38a27238b9cb36765
Priyanshu Kumar <priyanshu19jul@gmail.com>
2026-01-20 18:19:45 +0530

    2 very basic  models

---
+++ ML1/mlfirst.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt       
+import pandas as pd                      # importing importanta datas
+dataset = pd.read_csv('Data.csv')
+x= dataset.iloc[:, :-1].values
+y= dataset.iloc[:,-1].values
+print(x)
+print(y)
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:,1:3])
+x[:,1:3]=imputer.transform(x[:,1:3])
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+print(x)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
---
+++ ML2/exercise5.py
@@ -0,0 +1,25 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+# Load the Wine Quality Red dataset
+df = pd.read_csv('winequality-red.csv',delimiter=';' )
+# Separate features and target
+
+# Split the dataset into an 80-20 training-test set
+
+X = df.drop('quality', axis=1) 
+y = df['quality'] 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create an instance of the StandardScaler class
+
+sc = StandardScaler()
+# Fit the StandardScaler on the features from the training set and transform it
+X_train = sc.fit_transform(X_train) 
+X_test = sc.transform(X_test)
+# Apply the transform to the test set
+
+# Print the scaled training and test datasets
+print("Scaled training set:\n", X_train) 
+print("Scaled test set:\n", X_test)
