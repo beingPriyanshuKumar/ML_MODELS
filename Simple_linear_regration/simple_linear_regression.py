25e44f8f6c1bf9625449dd0397de046da2542b62
Priyanshu Kumar <priyanshu19jul@gmail.com>
2026-01-22 18:35:36 +0530

    simple regression model

--- Simple_linear_regration/simple_linear_regression.py
+++ Simple_linear_regration/simple_linear_regression.py
@@ -12,6 +12,23 @@ y = df.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+#traing the simple regression model on training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+#predicting the training set results
+y_pred=regressor.predict(X_test)
+plt.scatter(X_train,y_train , color='red')
+plt.plot(X_train,regressor.predict(X_train), color='blue' )
+plt.xlabel('years of experince')
+plt.ylabel('salary')
+plt.show()
+
+#predicting the test set results
+y_pred=regressor.predict(X_test)
+plt.scatter(X_test,y_test , color='red')
+plt.plot(X_train,regressor.predict(X_train), color='blue' )
+plt.xlabel('years of experince')
+plt.ylabel('salary')
+plt.show()
